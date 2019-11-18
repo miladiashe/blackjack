b7718f2bd0fb4d6d9b93a19803526cfc8d649d28
miladiashe <55428028+miladiashe@users.noreply.github.com>
2019-11-18 14:24:14 +0900

    이제야 시작
    별도의 .c 파일에 분할하는 걸 알아내야 시작할 수 있을 것 같아서 그거부터 연습하는 중

---
+++ headtest.h
@@ -0,0 +1,1 @@
+int function_test(int a, int b);
---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "headtest.h"
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) { 
+	/*1-게임의 전체적인 흐름에 따라  코드의 뼈대를 짤 것*/
+		/*1. 블랙잭 게임의 흐름을 제대로 이해하자-난 아직까지 1대1밖에 생각을 못 한다*/
+	/*2- 세부적 구현방법- 정의서에 명시된 것/
+		/*1. 다른 .c 파일에 담긴 함수 정의를 불러오는 법을 공부하기*/
+			/*테스트*/ 
+			int numb;
+			numb=function_test(3, 4);
+			printf("function_test(3, 4)=%d", numb);
+	/*3-세부적 구현방법- 만들다 필요해진 것*/ 
+	
+	return 0;
+}
