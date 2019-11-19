3041c9c0a0284750b1981b091a3eec2fb0c813dc
miladiashe <55428028+miladiashe@users.noreply.github.com>
2019-11-20 00:54:40 +0900

    checksum 구현

--- givecards.c
+++ givecards.c
@@ -12,13 +12,18 @@ int checksum21(int a)
 /*합이 21인지 체크해야 하는 부분에 일일이 체크하는 대신 checksum 함수를 쓸 것이다.*/
 {
 	extern mycardsum[];
-	if (mycardsum[a]=21)
+	if (mycardsum[a]>21)
 	{
-		return 21;
+		return 1;
 	}
-	else
+	else if (mycardsum[a]<21)
 	{
 		return 0;
 	}
-	/*if  mycardsum[n]이 21일 경우 그 플레이어는 블랙잭이다. 블랙잭 여부를 구현하려면?*/
+	else
+	{
+		return 21;
+	 }
+	/*if  mycardsum[n]이 21일 경우 그 플레이어는 블랙잭이다. 블랙잭 여부를 구현하려면?
+	mycardsum[a] 를 이용, 이 함수의 힙에 따라  0일 시 아무것도 아님, 1일 시 21 초과로 게임 오버, 2일 시 카드 받기 중지 , 21일 시 블랙잭 의 값을 돌려준다.*/
  } 
