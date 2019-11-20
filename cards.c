25cf9cbaea9610a176f6cbcafffb2d61f0b41055
miladiashe <55428028+miladiashe@users.noreply.github.com>
2019-11-20 12:32:53 +0900

    카드 저장방식 변경

--- cards.c
+++ cards.c
@@ -4,63 +4,63 @@
 #include "define.h"
 
 void makecards(){
-	extern char *trump[];
-	extern char *cardtray[];
+	extern int trump[];
+	extern int cardtray[];
 	if (N_CARD<52)
 	printf("잃어버린 카드를 찾아야 합니다\n");
 	else{
-		trump[0] = "spade A";
-		trump[1] = "spade 2";
-		trump[2] = "spade 3";
-		trump[3] = "spade 4";
-		trump[4] = "spade 5";
-		trump[5] = "spade 6";
-		trump[6] = "spade 7";
-		trump[7] = "spade 8";
-		trump[8] = "spade 9";
-		trump[9] = "spade 10";
-		trump[10] = "spade J";
-		trump[11] = "spade Q";
-		trump[12] = "spade K";
-		trump[13] = "diamond A";
-		trump[14] = "diamond 2";
-		trump[15] = "diamond 3";
-		trump[16] = "diamond 4";
-		trump[17] = "diamond 5";
-		trump[18] = "diamond 6";
-		trump[19] = "diamond 7";
-		trump[20] = "diamond 8";
-		trump[21] = "diamond 9";
-		trump[22] = "diamond 10";
-		trump[23] = "diamond J";
-		trump[24] = "diamond Q";
-		trump[25] = "diamond K";
-		trump[26] = "heart A";
-		trump[27] = "heart 2";
-		trump[28] = "heart 3";
-		trump[29] = "heart 4";
-		trump[30] = "heart 5";
-		trump[31] = "heart 6";
-		trump[32] = "heart 7";
-		trump[33] = "heart 8";
-		trump[34] = "heart 9";
-		trump[35] = "heart 10";
-		trump[36] = "heart J";
-		trump[37] = "heart Q";
-		trump[38] = "heart K";
-		trump[39] = "club A";
-		trump[40] = "club 2";
-		trump[41] = "club 3";
-		trump[42] = "club 4";
-		trump[43] = "club 5";
-		trump[44] = "club 6";
-		trump[45] = "club 7";
-		trump[46] = "club 8";
-		trump[47] = "club 9";
-		trump[48] = "club 10";
-		trump[49] = "club J";
-		trump[50] = "club Q";
-		trump[51] = "club K";
+		trump[0] = 1;
+		trump[1] = 2;
+		trump[2] = 3;
+		trump[3] = 4;
+		trump[4] = 5;
+		trump[5] = 6;
+		trump[6] = 7;
+		trump[7] = 8;
+		trump[8] = 9;
+		trump[9] = 10;
+		trump[10] = 11;
+		trump[11] = 12;
+		trump[12] = 13;
+		trump[13] = 1001;
+		trump[14] = 1002;
+		trump[15] = 1003;
+		trump[16] = 1004;
+		trump[17] = 1005;
+		trump[18] = 1006;
+		trump[19] = 1007;
+		trump[20] = 1008;
+		trump[21] = 1009;
+		trump[22] = 1010;
+		trump[23] = 1011;
+		trump[24] = 1012;
+		trump[25] = 1013;
+		trump[26] = 2001;
+		trump[27] = 2002;
+		trump[28] = 2003;
+		trump[29] = 2004;
+		trump[30] = 2005;
+		trump[31] = 2006;
+		trump[32] = 2007;
+		trump[33] = 2008;
+		trump[34] = 2009;
+		trump[35] = 2010;
+		trump[36] = 2011;
+		trump[37] = 2012;
+		trump[38] = 2013;
+		trump[39] = 3001;
+		trump[40] = 3002;
+		trump[41] = 3003;
+		trump[42] = 3004;
+		trump[43] = 3005;
+		trump[44] = 3006;
+		trump[45] = 3007;
+		trump[46] = 3008;
+		trump[47] = 3009;
+		trump[48] = 3010;
+		trump[49] = 3011;
+		trump[50] = 3012;
+		trump[51] = 3013;
 	/*기준이 되는 카드 한 벌.*/
 	
 	int tempcard;
@@ -83,12 +83,12 @@ void mixcards()
 	srand((unsigned)time(NULL));
 	/*원래 여기 둬야 할걸 다른 함수에 두고 잊어버렸었다. 시간 기반해서 랜덤함수의 시드를 심자*/
 	int bfmix, afmix;
-	char *tmpcard;
-	extern char *cardtray[];
+	int tmpcard;
+	extern int cardtray[];
 	/*임시변수*/ 
-	for(bfmix=0; bfmix<N_CARDSET*N_CARD; bfmix++)
+	for(bfmix=0; bfmix<(N_CARDSET*N_CARD); bfmix++)
 	{
-		afmix = rand()%N_CARDSET*N_CARD;
+		afmix = rand()%(N_CARDSET*N_CARD);
 		tmpcard = cardtray[bfmix];
 		cardtray[bfmix] = cardtray[afmix];
 		cardtray[afmix] = tmpcard; 
--- main.c
+++ main.c
@@ -22,10 +22,9 @@ int bet[N_maxplayer];
 /*베팅한 액수를 담아 두는 변수*/ 
 int gameover = 0;
 /*게임이 완전히 끝났음을 알리는 변수*/  
-
-char *trump[N_CARD];
+int trump[N_CARD];
 /*카드 한 벌*/ 
-char *cardtray[N_CARDSET*N_CARD];
+int cardtray[N_CARDSET*N_CARD];
 /*N_CARDSET벌의  카드를 쓰는 카드 한뭉치*/  
 
 
@@ -43,19 +42,19 @@ int i;
 	playerconfig();
 		/*player 수를 설정한다. 돈도 미리 준다.*/ 
 	makecards();
-	/*디버깅을 위해 삽입한 부분. 카드가 잘 만들어졌는지 확인하려면 이 부분을 주석에서 뺴내보자. 
+	/*디버깅을 위해 삽입한 부분. 카드가 잘 만들어졌는지 확인하려면 이 부분을 주석에서 뺴내보자.
 			for (i=0; i<52; i++)
 			{
-				printf ("%s\n", cardtray[i]);
+				printf ("%i\n", cardtray[i]);
 			}
 		/*카드 수를 N_CARDSET 만큼  찍어내는 함수*/
 	mixcards();
-	/*디버깅을 위해 삽입한 부분. 카드가 섞이는지 확인하려면 이 부분을 주석에서 뺴내보자. 
+	/*디버깅을 위해 삽입한 부분. 카드가 섞이는지 확인하려면 이 부분을 주석에서 뺴내보자.
 		for (i=0; i<52; i++)
 		{
-			printf ("%s\n", cardtray[i]);
-		}*/
-		/*카드를 섞는다-포인터 배열로 카드 섞는 순서 정하기?*/
+			printf ("%i\n", cardtray[i]);
+		}
+		/*카드를 섞는다*/
 	do{
 		
 		/*여기서 한번 반복문 시작- 한 라운드를 나타낸다.*/
